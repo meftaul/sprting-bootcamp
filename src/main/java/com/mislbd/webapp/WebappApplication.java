e17ceda62ae915a2a8734061b3a6e300ef951a0d
Meftaul <meftaulhaque@gmail.com>
2025-06-10 22:24:49 +0600

    1st Commit

---
+++ src/main/java/com/mislbd/webapp/WebappApplication.java
@@ -0,0 +1,14 @@
+package com.mislbd.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+		System.out.println("Hello Spring Boot.");
+	}
+
+}
